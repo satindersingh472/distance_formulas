2660f8b9e8ab2dfe7dca6236a0d5fc03a1c5f447
satinder <satindersingh472@gmail.com>
2023-02-12 22:27:07 -0700

    finihsed

--- app.py
+++ app.py
@@ -6,16 +6,37 @@ def euclidean_distance(pt1, pt2):
     distance += (pt1[i] - pt2[i]) ** 2
   return distance ** 0.5
 
-print(euclidean_distance([1, 2], [4, 0]))
-print(euclidean_distance([5, 4, 3], [1, 7, 9]))
-
 
 def manhattan_distance(pt1,pt2):
   distance = 0
   for i in range(len(pt1)):
     distance = distance + abs(pt1[i] - pt2[i])
   return distance
+  
+def hamming_distance(pt1,pt2):
+  distance = 0
+  for i in range(len(pt1)):
+    if (pt1[i] != pt2[i]):
+      distance += 1
+  return distance
 
+print("hamming distance for [1,2],[1,100]")
+print(hamming_distance([1,2],[1,100]))
+print("hamming distance for [5,4,9],[1,7,9]")
+print(hamming_distance([5,4,9],[1,7,9]))
 
+print('--------------------------------')
+
+print('manhattan distance for [1,2],[4,0]')
 print(manhattan_distance([1, 2], [4, 0]))
+
+print('manhattan distance for [5,4,3],[1,7,9,]')
 print(manhattan_distance([5, 4, 3], [1, 7, 9]))
+
+print('----------------------------------')
+
+print('euclidean distance for [1,2],[4,0]')
+print(euclidean_distance([1, 2], [4, 0]))
+
+print('euclidean distance for [5,4,3],[1,7,9,]')
+print(euclidean_distance([5, 4, 3], [1, 7, 9]))
